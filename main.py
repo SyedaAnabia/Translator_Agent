3c7d6e5433e0d276925b452b003f263c6cac2f19
SyedaAnabia <samdrive30@gmail.com>
2025-05-26 14:54:04 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,49 @@
+import os
+from agents import OpenAIChatCompletionsModel, AsyncOpenAI, Agent, Runner, set_tracing_disabled, RunConfig
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+set_tracing_disabled(True)
+
+OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
+
+# Initialize the OpenAI client
+client = AsyncOpenAI(
+    api_key="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",  # make sure to move this key to .env!
+    base_url="https://openrouter.ai/api/v1/"
+)
+
+# Initialize the agent with the correct model and pass client as 'openai_client'
+Translator = Agent(
+    model=OpenAIChatCompletionsModel(
+        model="qwen/qwen3-235b-a22b:free",
+        openai_client=client
+    ),
+    name="Translator ",
+    instructions="""  You are a translator AI. Your task is to translate text between English and Urdu only.
+    If the input is in English, translate it to Urdu.
+    If the input is in Urdu, translate it to English.
+    Only return the translated sentence. Do not add any extra explanation.
+    If the input is not in English or Urdu, respond with 'I can only translate between English and Urdu.""",
+)
+
+
+# Define the configuration for the agent
+
+config=RunConfig(
+     model=OpenAIChatCompletionsModel(
+        model="qwen/qwen3-235b-a22b:free",  # Ensure this model is available in your OpenRouter account
+        openai_client=client),
+    model_provider="client",
+    tracing_disabled=True,
+)
+
+# Run the agent
+result=Runner.run_sync(
+    Translator,
+    input= input("Enter text to translate (English or Urdu): "),
+    run_config=config
+
+)
+print(result.final_output)
